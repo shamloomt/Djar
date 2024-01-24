bf22c9fda7c44954bab80b9b33f06910facadde1
shamloomt <149014151+shamloomt@users.noreply.github.com>
2024-01-25 01:32:42 +0330

    Update settings.py

--- tavallodeno/settings.py
+++ tavallodeno/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'users',
     'home',
     'django_jalali',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'tavallodeno.urls'
